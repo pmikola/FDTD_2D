215a5b7d263cf1d224906e59bc67f70bb9556e05
pmikola <pio.mikolajczyk@gmail.com>
2023-09-26 21:03:51 +0200

    final_version

--- MMI_sim_4_25.py
+++ MMI_sim_4_25.py
@@ -17,17 +17,11 @@ from numba import prange, jit
 from C import C
 from numba import cuda
 from numba import vectorize
+
 # from vispy import plot as vp
 
 # mpl.use('Agg')
 jit(device=True)
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("GPU is available and being used")
-else:
-    device = torch.device("cpu")
-    print("GPU is not available, using CPU instead")
-
 s = time.time()
 nett_time_sum = 0
 np.seterr(divide='ignore', invalid='ignore')
@@ -44,10 +38,26 @@ def data_type(data, flag):
         return np.float64(data)
 
 
+def in_port_len(wg_input_length, port_range, port_offset_start, port_offset_stop):
+    inputy_start = int(wg_input_length - port_range) + port_offset_start
+    inputy_stop = int(wg_input_length) + port_offset_stop
+    input_meas_port_range = np.arange(inputy_start, inputy_stop, 1)
+    return input_meas_port_range
+
+
+def out_port_len(wg_output_start, port_range):
+    measx_start = int(wg_output_start)
+    measx_stop = int(wg_output_start + port_range)
+    output_meas_port_range = np.arange(measx_start, measx_stop, 1)
+    return output_meas_port_range
+
+
 # -------------------------------- KERNELS ---------------------------
-z = 10
+dx = 0.05  # each grid step is dx [um]
+dx_factor = 10 * dx
+z = 10 / dx_factor
+
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Ez_inc_CU(ez_inc, hx_inc):
     for j in prange(1, JE):
         for i in prange(0, IE):
@@ -74,7 +84,6 @@ def Dz_CU(dz, hx, hy, gi2, gi3, gj2, gj3):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Hy_inc_CU(hy, ez_inc):
     # for j in prange(1, JE):
     for j in prange(ja, jb + 1):
@@ -84,7 +93,6 @@ def Hy_inc_CU(hy, ez_inc):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Dz_inc_val_CU(dz, hx_inc):
     for i in prange(ia, ib):
         dz[i, ja] = dz[i, ja] + 0.5 * hx_inc[i, ja - 1]
@@ -93,7 +101,6 @@ def Dz_inc_val_CU(dz, hx_inc):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Ez_Dz_CU(ez, ga, gb, dz, iz):
     for j in prange(0, JE):
         for i in prange(0, IE):
@@ -107,7 +114,6 @@ def Ez_Dz_CU(ez, ga, gb, dz, iz):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Hx_inc_CU(hx_inc, ez_inc):
     for j in prange(0, JE - 1):
         for i in prange(0, IE):
@@ -119,7 +125,6 @@ def Hx_inc_CU(hx_inc, ez_inc):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Hx_CU(ez, hx, ihx, fj3, fj2, fi1):
     for j in prange(0, JE - 1):
         for i in prange(0, IE - 1):
@@ -134,7 +139,6 @@ def Hx_CU(ez, hx, ihx, fj3, fj2, fi1):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Hx_inc_val_CU(hx, ez_inc):
     # for j in prange(0, JE):
     for i in prange(ia, ib + 1):
@@ -144,7 +148,6 @@ def Hx_inc_val_CU(hx, ez_inc):
 
 
 @jit(nopython=True, parallel=True)
-# @numba.jit(nopython=True, parallel=True)
 def Hy_CU(hy, ez, ihy, fi3, fi2, fi1):
     for j in prange(0, JE):
         for i in prange(0, IE - 1):
@@ -158,8 +161,7 @@ def Hy_CU(hy, ez, ihy, fi3, fi2, fi1):
     return ihy, hy
 
 
-# @jit(nopython=True, parallel=True)
-@numba.jit(nopython=True, parallel=True)
+@jit(nopython=True, parallel=True)
 def Power_Calc(Pz, ez, hy, hx):
     for j in prange(0, JE):
         for i in prange(0, IE):
@@ -171,9 +173,9 @@ def Power_Calc(Pz, ez, hy, hx):
 cc = C()
 flag = 1
 data_type1 = np.float32
-IE = 1000  # y
-JE = 1000  # x
-npml = 8
+IE = 1500  # y
+JE = 1500  # x
+npml = int(8 / dx_factor)
 NFREQS = 3
 freq = [data_type(0, 1)] * NFREQS
 for n in range(0, NFREQS):
@@ -183,23 +185,25 @@ arg = [data_type(0, flag)] * NFREQS
 # ddx = data_type((cc.c0 / min(freq) / (10 * cc.nSiO2)), flag)  # Cells Size
 # n=speed in vaccum/speed in medium
 
-n_index = cc.nGe
+n_index = 3.6  # cc.nGe # effective index from equation or fimmwave
 n_sigma = 0.  # cc.sigmaSiO2
 epsilon = data_type(n_index, flag)
 sigma = data_type(n_sigma, flag)
 epsilon_medium = data_type(1.003, flag)
-sigma_medium = data_type(1., flag)
+sigma_medium = data_type(0., flag)
 
 # cc.c0 / (n_index * (min(freq)))
 A = 1.
 vm = cc.wavelength * (min(freq))
 # vm = cc.c0 / cc.nGe
-dx = 0.1  # each grid step is dx [um]
 # wavelength = (vm / (min(freq)))
 ddx = data_type(cc.wavelength * dx, flag)  # Cells Size
 # dt = 1 / (cc.c0 * np.sqrt((1 / ddx)**2 + (1 / ddx)**2))
 # dt = data_type((ddx / cc.c0) * M.sqrt(2), flag)  # Time step
 dt = ddx / (2 * cc.c0)  # Working moderate but ok
+correction = 0.5
+second_correction = 11.85
+wave_meas = 4.25 / dx + 1 / dx_factor
 
 #   CFL stability condition- Lax Equivalence Theorem
 # dt = 1 / (vm * M.sqrt(1 / (ddx ** 2) + 1 / (ddx ** 2)))  # Time step
@@ -211,19 +215,16 @@ epsz = data_type(8.854E-12, flag)
 spread = data_type(8, flag)
 t0 = data_type(1, flag)
 # print(ic.shape)
-ic = IE / 2
-jc = JE / 2
-ia = 7  # total scattered field boundaries
+ia = int(7 / dx_factor)  # total scattered field boundaries
 ib = IE - ia - 1
 ja = ia
 jb = JE - ja - 1
 T = 0
-zero_range = 3  # 5
-zeroing_ezinc = 2
+zero_range = int(3 / dx_factor)  # 5
+zeroing_ezinc = int(2 / dx_factor)
 medium_eps = 1. / (epsilon_medium + sigma_medium * dt / epsz)
 medium_sigma = sigma_medium * dt / epsz
-k_vec = 2 * M.pi / cc.wavelength
-omega = 2 * M.pi * freq[0]
+
 # print(dt)
 for n in range(0, NFREQS):
     arg[n] = 2 * M.pi * freq[n] * dt
@@ -234,34 +235,34 @@ ez_inc_low_m1 = data_type(0., flag)
 ez_inc_high_m2 = data_type(0., flag)
 ez_inc_high_m1 = data_type(0., flag)
 
-dz = tf.zeros((IE, JE), dtype=data_type1)
-iz = tf.zeros((IE, JE), dtype=data_type1)
-ez = tf.zeros((IE, JE), dtype=data_type1)
-hx = tf.zeros((IE, JE), dtype=data_type1)
-hy = tf.zeros((IE, JE), dtype=data_type1)
-ihx = tf.zeros((IE, JE), dtype=data_type1)
-ihy = tf.zeros((IE, JE), dtype=data_type1)
-ga = tf.ones((IE, JE), dtype=data_type1) * medium_eps  # main medium epsilon
-gb = tf.ones((IE, JE), dtype=data_type1) * medium_sigma  # main medium sigma
-Pz = tf.zeros((IE, JE), dtype=data_type1)
+dz = np.zeros((IE, JE), dtype=data_type1)
+iz = np.zeros((IE, JE), dtype=data_type1)
+ez = np.zeros((IE, JE), dtype=data_type1)
+hx = np.zeros((IE, JE), dtype=data_type1)
+hy = np.zeros((IE, JE), dtype=data_type1)
+ihx = np.zeros((IE, JE), dtype=data_type1)
+ihy = np.zeros((IE, JE), dtype=data_type1)
+ga = np.ones((IE, JE), dtype=data_type1) * medium_eps  # main medium epsilon
+gb = np.ones((IE, JE), dtype=data_type1) * medium_sigma  # main medium sigma
+Pz = np.zeros((IE, JE), dtype=data_type1)
 
-gi2 = tf.ones(IE, dtype=data_type1)
-gi3 = tf.ones(IE, dtype=data_type1)
-fi1 = tf.zeros(IE, dtype=data_type1)
-fi2 = tf.ones(IE, dtype=data_type1)
-fi3 = tf.ones(IE, dtype=data_type1)
+gi2 = np.ones(IE, dtype=data_type1)
+gi3 = np.ones(IE, dtype=data_type1)
+fi1 = np.zeros(IE, dtype=data_type1)
+fi2 = np.ones(IE, dtype=data_type1)
+fi3 = np.ones(IE, dtype=data_type1)
 
-gj2 = tf.ones(JE, dtype=data_type1)
-gj3 = tf.ones(JE, dtype=data_type1)
-fj1 = tf.zeros(JE, dtype=data_type1)
-fj2 = tf.ones(JE, dtype=data_type1)
-fj3 = tf.ones(JE, dtype=data_type1)
+gj2 = np.ones(JE, dtype=data_type1)
+gj3 = np.ones(JE, dtype=data_type1)
+fj1 = np.zeros(JE, dtype=data_type1)
+fj2 = np.ones(JE, dtype=data_type1)
+fj3 = np.ones(JE, dtype=data_type1)
 
-ez_inc = tf.zeros((IE, JE), dtype=data_type1)
-hx_inc = tf.zeros((IE, JE), dtype=data_type1)
+ez_inc = np.zeros((IE, JE), dtype=data_type1)
+hx_inc = np.zeros((IE, JE), dtype=data_type1)
 
 # PML Definition
-alpha = 0.333333
+alpha = 0.33333
 for i in range(npml):
     xnum = npml - i
     xd = npml
@@ -325,15 +326,14 @@ cr.paint()
 
 # 2x2 MMI 4.25um PARAMETERS
 ############################
-waveguide_width = 20
-mmi_width = 63
-mmi_length = 700
+waveguide_width = correction * 20 / dx_factor
+mmi_width = correction * 63 / dx_factor
+mmi_length = correction * 700 / dx_factor
 mmi_left_corner = IE / 2 - mmi_width / 2
-wg_offset = 5
-wg_input_length = 100
-tapers_length = 50
-temp_offset = 100
-taper_width_offset = 3
+wg_offset = correction * 4 / dx_factor
+wg_input_length = correction * 200 / dx_factor
+tapers_length = correction * 50 / dx_factor
+taper_width_offset = correction * 5 / dx_factor
 ############################
 wg_top_left_corner = mmi_left_corner + wg_offset
 wg_bottom_left_corner = mmi_left_corner + mmi_width - waveguide_width - wg_offset
@@ -406,19 +406,17 @@ for j in range(0, shape2):
             pass
             # print(data[i, j, 0])
 
-inputy_start = int(ja + 50)
-inputy_stop = int(wg_input_length)
-input_meas_port_range = np.arange(inputy_start, inputy_stop, 1)
+port_range = wave_meas
+port_offset_start = 0
+port_offset_stop = 0
+port_width_start = int(mmi_left_corner + mmi_width / 2 - mmi_width * 2)
+port_width_stop = int(mmi_left_corner + mmi_width / 2 + mmi_width * 2)
 
-measx_start = int(wg_output_start)
-measx_stop = int(jb - 50)
-output_meas_port_range = np.arange(measx_start, measx_stop, 1)
-
-measy = int(IE * 0.3)
-probey = range(measy, int(IE - IE * 0.3))
-probex_old = [measx_start] * len(probey)
-probex_out = [output_meas_port_range] * len(probey)
-probex_in = [input_meas_port_range] * len(probey)
+probey = range(port_width_start, port_width_stop)
+input_meas_port_range = in_port_len(wg_input_length, port_range, port_offset_start, port_offset_stop)
+output_meas_port_range = out_port_len(wg_output_start, port_range)
+probex_out = [output_meas_port_range] * (len(probey))
+probex_in = [input_meas_port_range] * (len(probey))
 
 INTEGRATE = []
 MaxField = []
@@ -433,14 +431,16 @@ y = np.linspace(0, IE, IE)
 # values = range(len(x))
 X, Y = np.meshgrid(x, y)
 
-fig = plt.figure(figsize=(10, 5))
+fig = plt.figure(figsize=(12, 6))
 grid = plt.GridSpec(20, 20, wspace=2, hspace=0.6)
 # ax = fig.add_subplot(grid[:, :5])
-ay = fig.add_subplot(grid[:, :10])
-az = fig.add_subplot(grid[:, 12:])
+ay = fig.add_subplot(grid[:10, :])
+az = fig.add_subplot(grid[12:, :])
 # Cyclic Number of image snapping
 frame_interval = 64
-nsteps = 100
+nsteps = 10000
+tstamp = 0
+stop_sim = 0
 for n in range(1, nsteps + 1):
     net = time.time()
     T += 1
@@ -480,49 +480,82 @@ for n in range(1, nsteps + 1):
     nett_time_sum += netend - net
 
     if T % frame_interval == 0:
+        imax = omax = 0.
+        pending = round(T * 100. / nsteps, 2)
         INTEGRATE.append(Pz)
-        YY = np.trapz(INTEGRATE, axis=0, dx=1.0) / window-1
+        YY = np.trapz(INTEGRATE, axis=0, dx=1.0) / window
         # print(YY.shape)
         # print(np.sum(YY,axis=1))
         if len(INTEGRATE) >= window:
             del INTEGRATE[0]
         # measure_port = np.abs(YY)[probey, probex]
-        measure_port_out = [0.] * len(probey)
-        measure_port_in = [0.] * len(probey)
+
+        measure_port_out = [0.] * (len(probey))
+        measure_port_in = [0.] * (len(probey))
         g = 0
         h = 0
+
         for g in range(0, len(np.array(probex_out)[0])):
             measure_port_out += np.abs(YY)[probey, np.array(probex_out)[:, g]]
+            omax += np.max(measure_port_out)
 
         for h in range(0, len(np.array(probex_in)[0])):
             measure_port_in += np.abs(YY)[probey, np.array(probex_in)[:, h]]
+            imax += np.max(measure_port_in)
+
+        imax /= len(np.array(probex_in)[0])
+        imax /= window * correction
+        omax /= len(np.array(probex_out)[0])
+        omax /= window * correction
 
         measure_port_out /= len(np.array(probex_out)[0])
-        measure_port_in /= len(np.array(probex_in)[0])
-        # if np.sum(measure_port_out) > 1e-2:
-        #     A = 1.5
-        # else:pass
+        measure_port_out /= window * correction
+        measure_port_out *= second_correction
 
-        # MaxField.append(measure_port)
-        # mf = np.sum(MaxField, axis=1)
-        # mf_id = np.argmax(mf)
-        # print(mf_id)
+        measure_port_in /= len(np.array(probex_in)[0])
+        measure_port_in /= window * correction
+        measure_port_in *= second_correction
+
         # title = ay.annotate("Time :" + '{:<.4e}'.format(T * dt * 1 * 10 ** 15) + " fs", (1, 0.5),
         #                     xycoords=ay.get_window_extent, xytext=(-round(JE * 2), IE - 5),
         #                     textcoords="offset points", fontsize=9, color='white')
         # ay.set(xlim=(-ic, ic), ylim=(-jc, jc))
+        # input_max = np.max(measure_port_in)
+        # if pending > 50 and input_max < 0.95:
+        #     port_offset_start -= 2
+        #     port_offset_stop -= 1
+        # elif pending > 50 and input_max > 1.05:
+        #     port_offset_start += 2
+        #     port_offset_stop += 1
+        # else:
+        #     pass
 
-        ims2 = ay.imshow(Pz, cmap=cm.nipy_spectral, extent=[0, JE, 0, IE])
-        ims2.set_interpolation('bilinear')
+        opoint = np.sum(measure_port_out)
+        # print(ppoint)
+        if opoint >= 10.:
+            stop_sim += 1
+            if stop_sim >= 15:  # int(IE / 100):
+                sys.stdout.write('\r')
+                sys.stdout.write("Pending...100 %")
+                sys.stdout.flush()
+                break
+        else:
+            pass
+
+        # cm.nipy_spectral
+        ims2 = ay.imshow(Pz, cmap=cm.nipy_spectral, extent=[0, JE, 0, IE], aspect='auto')
+        ay.set_ylim([IE * 0.4, IE * 0.6])
+        # ims2.set_interpolation('kaiser')
         ims4 = ay.scatter(x_points, y_points, c='grey', s=1, alpha=0.01)
         ims_oport_start = ay.scatter(np.array(probex_out)[:, 0], probey, c='red', s=5, alpha=0.05)
         ims_oport_stop = ay.scatter(np.array(probex_out)[:, g], probey, c='red', s=5, alpha=0.05)
         ims_iport_start = ay.scatter(np.array(probex_in)[:, 0], probey, c='g', s=5, alpha=0.05)
         ims_iport_stop = ay.scatter(np.array(probex_in)[:, h], probey, c='g', s=5, alpha=0.05)
         # ims6, = az.plot(MaxField[mf_id], 'r')  # field distribiution
+
         ims_out, = az.plot(measure_port_out, 'r', alpha=0.85)
         ims_in, = az.plot(measure_port_in, '-.g', alpha=0.85)
-        ppoint = len(measure_port_out)
+        az.set_ylim([-0.1, 1.1])
 
         # FFT CALCULATION
         # fft_out = fft.fftshift(fft.fft(torch.from_numpy(YY[:, measx])))
@@ -533,10 +566,11 @@ for n in range(1, nsteps + 1):
         # fft_result = 2.0 / len(fft_res) * torch.abs(fft_res[0:len(fft_res) // 2])
         # ims7, = ax.plot(fft_len, fft_result, 'g')
         # FFT CALCULATION
+
         ims.append([ims2, ims4, ims_oport_start, ims_oport_stop, ims_iport_start, ims_iport_stop, ims_out,
                     ims_in])  # , title])
         sys.stdout.write('\r')
-        sys.stdout.write("Pending..." + str(round(T * 100. / nsteps, 2)) + " %")
+        sys.stdout.write("Pending..." + str(pending) + " %")
         sys.stdout.flush()
 
 # ax.set_xscale('log')
@@ -545,26 +579,31 @@ ay.set_xlabel("x [um]")
 ay.set_ylabel("y [um]")
 # ax.set_xlabel("Frequency [Hz]")
 # ax.set_ylabel("Power [W]")
-az.set_xlabel("x [um]")
+az.set_xlabel("y [um]")
 az.set_ylabel("Pfwd")
 
 xlabels = [item.get_text() for item in ay.get_xticklabels()]
 ylabels = [item.get_text() for item in ay.get_yticklabels()]
-xlab = [float(x) * dx for x in xlabels]
-ylab = [float(y) * dx - dx * IE / 2 for y in ylabels]
+xlab = [float(x) * dx / correction for x in xlabels]
+ylab = [float(y) * dx / correction - (dx * IE / 2) / correction for y in ylabels]
 ay.set_xticklabels(xlab)
 ay.set_yticklabels(ylab)
 
 az.grid(True)
-az.locator_params(axis='x', nbins=5)
+az.locator_params(axis='x', nbins=6)
 zlabels = [item.get_text() for item in az.get_xticklabels()]
-zlab = [float(z) * dx - float(zlabels[-1]) * dx / 2 for z in zlabels[1:]]
-zlab_bins = np.linspace(start=zlab[0], stop=zlab[-1], num=5)
+zlab = [float(z) * dx / correction - (float(zlabels[-1]) * dx / 2) / correction for z in zlabels[1:]]
+
+zfin = [float(z)*dx for z in zlabels[1:-1]]
+
+
+zlab_bins = np.arange(start=-zfin[-1], stop=zlab[-1], step=2*zlab[-1]/6)
+# print(zlab_bins )
 # az.axis( xmin = zlab[0], xmax = zlab[-1])
 # az.tick_params(axis='x', which='major', labelsize=8)
 # az.set_xticks([zlab_bins[0],zlab_bins[1],zlab_bins[2],zlab_bins[3],zlab_bins[4]])
-
-az.set_xticklabels([' ', zlab_bins[0], zlab_bins[1], zlab_bins[2], zlab_bins[3], zlab_bins[4]])
+# print(zlab_bins)
+az.set_xticklabels([' ',str(zlab_bins[0]), str(zlab_bins[1]), str(zlab_bins[2]), str(zlab_bins[3]), str(zlab_bins[4]), str(zlab_bins[5])])
 # az.set_xticklabels(az.xaxis.get_majorticklabels())#, rotation=90)
 az.legend(['out', 'in'], loc='best')
 
@@ -577,14 +616,10 @@ print("Time netto SUM : " + str(nett_time_sum) + "[s]")
 file_name = "2d_fdtd_MMI_4.25um"
 # file_name = "./" + file_name + '.gif'
 file_name = "./" + file_name + '.gif'
-ani = animation.ArtistAnimation(fig, ims, interval=30, blit=True)  # , repeat=False)
+ani = animation.ArtistAnimation(fig, ims, interval=30, blit=True  , repeat=False)
 # ani.save(file_name, writer='pillow', fps=30, dpi=100)
 # ani.save(file_name + '.mp4', fps = 30, extra_args = ['-vcodec', 'libx264'])
 
 # ani.save(file_name, writer="imagemagick", fps=30)
 print("Plotting...")
 plt.show()
-
-# TODO : Check if the result are correct
-# TODO : Results in VisPy
-# TODO : Index profile of the medium
